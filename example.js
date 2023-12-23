ef4b5a40abafc95138645beaf1ab2b0181c9999d
Nino Tchumburidze <ninotch03@gmail.com>
2023-12-23 21:37:29 +0400

    Manual Version

--- example.js
+++ example.js
@@ -2,8 +2,13 @@ let gridType = "Medium";
 const board = document.getElementById("board");
 
 let startingAmount = 2;
+let betAmount = 2;
+let credit = 9999.00;
 let hasBeenClicked = 0;
-let index = 1.5;
+let index = 1;
+let success = false;
+let started = false;
+let cashedOut = false;
 
 //Grid adjustments
 let w = 7;
@@ -12,7 +17,9 @@ const board1 = document.getElementById("board1");
 const gapSize = 5;
 const tiles = document.querySelectorAll("#board1 .tile");
 let tilew = 5;
+let tileh = 5;
 
+let clickedOnBomb = false;
 
 window.onload = function(){
     drawBoard();
@@ -23,36 +30,157 @@ window.onload = function(){
 
 }
 
-function nextColumn(n){
-    const arrow =  document.getElementById("arrow");
+function nextColumn(n) {
+    if(n===w+1){
+        credit = credit + parseFloat(startingAmount);
+        document.getElementById("money-amount").innerText = credit.toFixed(2)+"$";
+        startAgain();
+    }
+
+    started = true;
+    const arrow = document.getElementById("arrow");
+    
+    const multiplierElement = document.getElementById(`multiplier-${n-1}`);
+    const multiplier = parseFloat(multiplierElement.innerText);
+
     arrow.style.display = 'flex';
-    arrow.style.left = parseInt(arrow.style.offset) + tilew + "px";
+    if (n > 1) {
+        arrow.style.left = arrow.offsetLeft + tilew + gapSize + "px";
+    }
+    const bomb = document.getElementById("bomb");
 
-        const selector = `#board1 .tile:nth-child(${w}n + ${n})`;
-        const tilesInColumn = document.querySelectorAll(selector);
+    // Hide the bomb at the beginning of each column
+    bomb.style.display = 'none';
 
-        let bombindex = Math.floor(Math.random()*h);
+    const selector = `#board1 .tile:nth-child(${w}n + ${n})`;
+    const tilesInColumn = document.querySelectorAll(selector);
 
-        tilesInColumn.forEach((tile, index) => {
-             tile.style.backgroundColor = "white";
-             
-             tile.addEventListener("click", function(){
-                
-                if(index===bombindex){
-                    tile.style.backgroundColor="red";
-                }else{
-                    nextColumn(n+1);
-                }
-             });
-        });
+    let bombindex = Math.floor(Math.random() * h);
+    let columnClicked = false;
+    
+    tilesInColumn.forEach((tile, index) => {
+        tile.style.backgroundColor = "white";
+
+        // Adding click event listener with 'once' option
+        tile.addEventListener("click", function handler() {
+            clickHandler(tile, index);
+        }, { once: true });
+    });
+
+    function clickHandler(tile, index) {
+        // If the user has already made a selection, do nothing
+        if (columnClicked) {
+            return;
+        }
+
+        // Hide the bomb when a tile is clicked
+        bomb.style.display = 'none';
+        columnClicked = true;
+
+        if (index === bombindex) {
+            const tileRect = tile.getBoundingClientRect();
+
+            // Set bomb position to be exactly in the clicked tile
+            bomb.style.left = tileRect.left + tilew / 4 + "px";
+            bomb.style.top = tileRect.top + tileh / 4 + "px";
+
+            tile.style.backgroundColor = "red";
+            bomb.style.display = 'flex';
+            clickedOnBomb = true;
+            document.getElementById("bet").innerText = "BET";
+            success = false;
+            startingAmount = betAmount;
+        } else {
+            clickedOnBomb = false;
+            startingAmount = (startingAmount*multiplier).toFixed(2);
+            document.getElementById("bet").innerText = "CASHOUT"+"("+startingAmount+")";
+            // Move to the next column
+            success=true;   
+            nextColumn(n + 1);
+        }
+    }
+
+   
 }
 
+function disableButtons() {
+    // Disable other buttons or elements as needed
+    const otherButtons = document.querySelectorAll("#circleElement1, #circleElement2, #oneDollar, #threeDollars, #fiveDollars, #tenDollars, #small, #medium, #big");
 
-function betButton(){
-    document.getElementById("bet").addEventListener("click", function(){
-        let n = 1;
-        nextColumn(n);
+    otherButtons.forEach(button => {
+        button.disabled = true;
     });
+}
+
+function enableButtons(){
+    const otherButtons = document.querySelectorAll("#circleElement1, #circleElement2, #oneDollar, #threeDollars, #fiveDollars, #tenDollars, #small, #medium, #big");
+
+    otherButtons.forEach(button => {
+        button.disabled = false;
+    });
+}
+function betButton() {
+    const betButtonElement = document.getElementById("bet");
+
+    betButtonElement.addEventListener("click", function () {
+        // Check if the game has started
+        if (!started) {
+            disableButtons()
+            started = true;
+            credit = credit - parseFloat(startingAmount);
+            document.getElementById("money-amount").innerText = credit.toFixed(2)+"$";
+            nextColumn(1);
+            
+            
+        }else if(clickedOnBomb){
+            enableButtons();
+            document.getElementById("money-amount").innerText = credit.toFixed(2) + "$";
+            startAgain();
+        }else if(success ===true){
+            enableButtons();
+            credit = credit + parseFloat(startingAmount);
+            document.getElementById("money-amount").innerText = credit.toFixed(2) + "$";
+            startAgain();
+        }
+
+       
+       
+    });
+}
+
+function startAgain(){
+    clearBoard();
+    drawBoard();
+    updateGrid();
+    started = false;
+    success = false;
+    document.getElementById("arrow").style.left = 710 + "px";
+    document.getElementById("bet").innerText = 'BET';
+    if(clickedOnBomb){
+        clickedOnBomb = false;
+        credit = credit - parseFloat(startingAmount);
+            document.getElementById("money-amount").innerText = credit.toFixed(2)+"$";
+        nextColumn(1);
+    }
+}
+
+function enableButtons(){
+    const otherButtons = document.querySelectorAll("#circleElement1, #circleElement2, #oneDollar, #threeDollars, #fiveDollars, #tenDollars, #small, #medium, #big");
+   
+    otherButtons.forEach(button => {
+        button.disabled =false;
+    });
+
+}
+
+function disableButtons() {
+    // Disable other buttons or elements as needed
+    const otherButtons = document.querySelectorAll("#circleElement1, #circleElement2, #oneDollar, #threeDollars, #fiveDollars, #tenDollars, #small, #medium, #big");
+
+    otherButtons.forEach(button => {
+          button.disabled = true;    
+    });
+
 }
 
 function clearBoard(){
@@ -67,6 +195,8 @@ function clearBoard(){
     while (board2.firstChild) {
         board2.removeChild(board2.firstChild);
     }
+    document.getElementById("arrow").style.display = 'none';
+    document.getElementById("bomb").style.display = 'none';
 }
 
 function drawBoard(){
@@ -100,6 +230,7 @@ function updateGrid() {
     const tileWidth = (board1.clientWidth - totalGapw) / w;
     const tileHeight = (board1.clientHeight - totalGaph) / h;
     tilew = tileWidth;
+    tileh = tileHeight;
 
     board1.style.gridTemplateColumns = `repeat(${w}, ${tileWidth}px)`;
     board2.style.gridTemplateColumns = `repeat(${w}, ${tileWidth}px)`;
@@ -113,7 +244,6 @@ function updateGrid() {
         tile.style.height = tileHeight + "px";
     });
 
-   
 
 }
 
@@ -162,7 +292,9 @@ function updateCustomAmount(value) {
 }
 
 function decrease() {
-    startingAmount -= 0.1;
+    betAmount = betAmount -amount;
+    betAmount = parseFloat(startingAmount.toFixed(2));
+    startingAmount = startingAmount- amount;
     startingAmount = parseFloat(startingAmount.toFixed(2));
     document.getElementById("amount").value = startingAmount.toFixed(2);
 }
@@ -170,18 +302,23 @@ function decrease() {
 
 function increaseAmount(amount) {
     if (amount === 0.1) {
-        startingAmount += amount;
+        startingAmount= startingAmount+ amount;
         startingAmount = parseFloat(startingAmount.toFixed(2));
+        betAmount = betAmount +amount;
+        betAmount = parseFloat(startingAmount.toFixed(2));
         document.getElementById("amount").value = startingAmount.toFixed(2);
         hasBeenClicked = 0.1;
     } else if (hasBeenClicked === amount) {
-        startingAmount += amount;
+        startingAmount= startingAmount+ amount;
         startingAmount = parseFloat(startingAmount.toFixed(2));
+        betAmount = betAmount + amount;
+        betAmount = parseFloat(startingAmount.toFixed(2));
         document.getElementById("amount").value = startingAmount.toFixed(2);
     } else {
         document.getElementById("amount").value = amount.toFixed(2);
         hasBeenClicked = amount;
         startingAmount = amount;
+        betAmount = amount;
     }
 }
 
